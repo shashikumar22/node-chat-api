c19200224004280099a20ee8f85f3c5bf1e8bde6
shashi <shashikumar2203@gmail.com>
2018-03-11 22:55:20 +0530

    Emit newmessage on createmessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,6 @@ socket.on('connect', function() {
   console.log('Connected to server');
 })
 
-socket.emit('createMessage', {
-  to: "Ken",
-  text: "Hey Ken",
-  createdAt: 123
-})
-
 socket.on('newMessage', function(data){
   console.log(`New message ${JSON.stringify(data)}`);
 })
--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.emit('newMessage',{
-    from: "John",
-    text: "Hey John",
-    createdAt: 123
-  })
-
-  socket.on('createMessage', (data) => {
-    console.log(data);
+  socket.on('createMessage', (message) => {
+    console.log('entered');
+    console.log(message);
+    
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text
+    })
   })
 
   socket.on('disconnect', () =>{
